b798a0bb5ea8ad08ac4b20cc88d0f35df21a8fd0
amit <amitoosh.mor@gmail.com>
2021-03-08 14:53:57 +0200

    PART A DONE
    3.7 3.8

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 app.use(express.json());
+// app.use(morgan("tiny"));
+morgan.token('type', function (req, res) { return JSON.stringify(req.body); });
+app.use (morgan(`:method :url :status :res[content-length] - :response-time ms :type`));
 let notes = [
     {
         id: 1,
