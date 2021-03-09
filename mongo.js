4f0e352bb05e5baec3276a9f848accb34f7a1289
amit <amitoosh.mor@gmail.com>
2021-03-09 19:52:32 +0200

    working with mongodb
    3.12

--- mongo.js
+++ mongo.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
-if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>');
-    process.exit(1);
-  }
+const arguments = process.argv.length;
+// switch(arguments){
+//     case arguments < 3 
+// }
+
+
+
   
   const password = process.argv[2];
   
@@ -18,21 +21,37 @@ if (process.argv.length < 3) {
   })
   
   const Person = mongoose.model('Person', personSchema);
-  
-//   const person = new Person({
-//     id: 5 ,
-//     name: "aaaaa",
-//     number: "444446",
-//   });
-  
-//   person.save().then(result => {
-//     console.log('person saved!' , person);
-//     mongoose.connection.close();
-//   })
+  function postPerson(){
+      const person = new Person({
+        id: Math.random()*10 ,
+        name: process.argv[3],
+        number: process.argv[4],
+      });
 
-Person.find({id:4}).then(result => {
-    result.forEach(Person => {
-      console.log(Person)
-    })
-    mongoose.connection.close()
-  });
+      person.save().then(result => {
+        console.log(` added ${person.name} , number: ${person.number} to phonebook`);
+        mongoose.connection.close();
+      });
+  }
+//   phonebook:
+// Anna 040-1234556
+// Arto Vihavainen 045-1232456
+// Ada Lovelace 040-1231236
+function showPeople(){
+    Person.find({}).then(result => {
+        console.log(result);
+        console.log("phonebook:");
+        result.forEach(person => {
+          console.log(` ${person.name}  ${person.number} `);
+        })
+        mongoose.connection.close()
+      });
+}
+if (arguments < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>');
+    process.exit(1);
+  }else if(arguments<5){
+      showPeople();
+   }else if(arguments === 5){
+       postPerson();
+   }
